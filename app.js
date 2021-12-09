37f9e1a57b4e4de59ae19a991522bd8e5b2a433e
Memon <s-memonh@bsd405.org>
2021-12-08 20:55:48 -0800

    beta version

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 const getElement = (sel) => document.querySelector(sel);
 const inputCanvas = getElement("#input-canvas");
 const outputCanvas = getElement("#output-canvas");
-const inputFOV = getElement("#input-angle");
-const outputFOV = getElement("#output-angle");
+const inputFOVs = document.querySelectorAll(".input-angle");
+const outputFOVs = document.querySelectorAll(".output-angle");
+const inputLabel = getElement("#input-label");
+const outputLabel = getElement("#output-label");
+const fovMode = getElement("#fov-mode");
+const fovOption = getElement("#fov-option");
+
 const inputCtx = inputCanvas.getContext("2d");
 const outputCtx = outputCanvas.getContext("2d");
 let image = null;
+let useDiagonalFOV = true;
 
+const validateInput = (input, fallback) => {
+  if (input == "") {
+    result = fallback;
+  } else {
+    result = parseInt(input);
+  }
+  if (isNaN(result)) {
+    throw new Error();
+  }
+  return result;
+}
 function getInputs() {
   let inputAngle, outputAngle;
   try {
-    inputAngle = inputFOV.value;
-    outputAngle = outputFOV.value;
-    if (inputAngle == "") {
-      inputAngle = 30;
+    if (useDiagonalFOV) {
+      inputAngle = validateInput(inputFOVs[0].value, 30);
+      outputAngle = validateInput(outputFOVs[0].value, 20);
     } else {
-      inputAngle = parseInt(inputAngle);
-    }
-    if (outputAngle == "") {
-      outputAngle = 20;
-    } else {
-      outputAngle = parseInt(outputAngle);
-    }
-    if (isNaN(inputAngle) || isNaN(outputAngle)) {
-      throw new Error();
+      inputAngle = [validateInput(inputFOVs[0].value, 30), validateInput(inputFOVs[1].value, 30)];
+      outputAngle = [validateInput(outputFOVs[0].value, 20), validateInput(outputFOVs[1].value, 20)];
     }
   } catch (err) {
+    console.error(err)
     alert("Invalid Values!");
-    inputAngle = temp[0];
-    outputAngle = temp[1];
+    if (useDiagonalFOV) {
+      inputAngle = 30;
+      outputAngle = 20;
+    } else {
+      inputAngle = [30, 30];
+      outputAngle = [20, 20];
+    }
   }
   return [inputAngle, outputAngle];
 }
@@ -49,17 +64,52 @@ function readImage() {
 
 const toRadians = (degrees) => (degrees * Math.PI) / 180;
 function getNewImageSize(inputAngle, outputAngle) {
-  const inputTan = Math.tan(toRadians(inputAngle / 2));
-  const outputTan = Math.tan(toRadians(outputAngle / 2));
-  const scaleFactor = outputTan / inputTan;
-  return [image.width * scaleFactor, image.height * scaleFactor, scaleFactor];
+  if (useDiagonalFOV) {
+    const inputTan = Math.tan(toRadians(inputAngle / 2));
+    const outputTan = Math.tan(toRadians(outputAngle / 2));
+    const scaleFactor = outputTan / inputTan;
+    return [image.width * scaleFactor, image.height * scaleFactor, scaleFactor];
+  } 
+  // Method 1
+  const inputTan = [
+    Math.tan(toRadians(inputAngle[0] / 2)), 
+    Math.tan(toRadians(inputAngle[1] / 2))
+  ];
+  const outputTan = [
+    Math.tan(toRadians(outputAngle[0] / 2)),
+    Math.tan(toRadians(outputAngle[1] / 2))
+  ];
+
+  const width = (image.width * outputTan[0]) / (2 * inputTan[0]);
+  const height = (image.height * outputTan[1]) / (2 * inputTan[1]);
+
+  //    Purcell method
+  // const distanceX = (image.width / 2) / (inputTan[0] * 2)
+  // const width = (distanceX * outputTan[0]) * 2;
+  // const distanceY = (image.height / 2) / (inputTan[1] * 2);
+  // const height = (distanceY * outputTan[1]) * 2;
+
+  console.log("Results: ", width, height, image.width, image.height);
+
+  return [width * 2, height * 2, null];
+
 }
 
 function update() {
   const [inputAngle, outputAngle] = getInputs();
-  if (inputAngle < outputAngle) {
-    alert("Input angle must be less than output angle!");
+  // validate angles
+  if (useDiagonalFOV) {
+    if (inputAngle < outputAngle) {
+      alert("Input angle must be less than output angle!");
+      return;
+    }
+  } else {
+    if (inputAngle[0] < outputAngle[0] || inputAngle[1] < outputAngle[1]) {
+      alert("Input angle must be less than output angle!");
+      return;
+    }
   }
+    
   const [width, height, scaleFactor] = getNewImageSize(inputAngle, outputAngle);
   const leftOffset = (image.width - width) / 2;
   const topOffset = (image.height - height) / 2;
@@ -72,9 +122,14 @@ function update() {
   const cropRect = [leftOffset, topOffset, width, height];
   inputCtx.strokeRect(...cropRect);
   outputCtx.drawImage(image, ...cropRect, 0, 0, width, height);
-  outputCanvas.style.width = scaleFactor * 100 + "%";
-  outputCanvas.style.marginTop = (img.height - height) / 2 + "px";
+  if (useDiagonalFOV) {
+    outputCanvas.style.width = scaleFactor * 100 + "%";
+  } else {
+    outputCanvas.style.width = (width / image.width) * 100 + "%";
+  }
+  outputCanvas.style.marginTop = (((inputCanvas.offsetHeight - outputCanvas.offsetHeight) / 2)) + "px";
 }
+
 
 function resizeCanvas(width, height, canvas) {
   canvas.width = width;
@@ -89,5 +144,28 @@ getElement("#image-upload").addEventListener("change", readImage);
 
 document.body.onload = () => {
   image = getElement("#temp");
-  update(30, 20);
+  update();
 };
+
+// UI TOGGLE 
+function toggleFOVMode() {
+  useDiagonalFOV = !useDiagonalFOV;
+  if (useDiagonalFOV) {
+    fovMode.innerHTML = "FOV mode: diagonal";
+    fovOption.innerHTML = "FOV mode: Vertical & Horizontal";
+    inputFOVs[1].style.display = "none";
+    outputFOVs[1].style.display = "none";
+    
+    inputLabel.innerHTML = "Input FOV angle: ";
+    outputLabel.innerHTML = "Output FOV angle: ";
+  } else {
+    fovMode.innerHTML = "FOV mode: Vertical & Horizontal";
+    fovOption.innerHTML = "FOV mode: diagonal";
+    inputFOVs[1].style.display = "block";
+    outputFOVs[1].style.display = "block";
+
+    inputLabel.innerHTML = "Input FOV: (horizontal) (vertical) ";
+    outputLabel.innerHTML = "Output FOV: (horizontal) (vertical) ";
+  }
+  update();
+}
